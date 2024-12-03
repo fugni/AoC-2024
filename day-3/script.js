c36fd3b8246bf3dd20d3992ea9f96f1bec67b501
schimmel <sylvienuijtemans@gmail.com>
2024-12-03 14:05:31 +0100

    day 3 part 2

--- day-3/script.js
+++ day-3/script.js
@@ -1,15 +1,33 @@
 fetch("input")
     .then((res) => res.text())
     .then((input) => {
-        const regex = /mul\(\-?\d+,\-?\d+\)/g
-        const valid_muls = input.match(regex);
+        const regex = /mul\(\-?\d+,\-?\d+\)|do\(\)|don't\(\)/g
+        const instructions = input.match(regex);
 
+        let enabled = 1
         let result = 0;
 
-        valid_muls.forEach(mul => {
-            var numbers = (mul.slice(4, mul.length - 1)).split(",")
-            var multiplied = parseInt(numbers[0]) * parseInt(numbers[1])
-            result += multiplied;
+        function handleInstructions(instruction) {
+            switch (instruction) {
+                case "do()":
+                    enabled = 1;
+                    break;
+                case "don't()":
+                    enabled = 0;
+                    break;
+                default:
+                    var numbers = (instruction.slice(4, instruction.length - 1)).split(",");
+                    numbers[0] = parseInt(numbers[0]), numbers[1] = parseInt(numbers[1]);
+                    if (enabled === 1) {
+                        var multiplied = numbers[0] * numbers[1]
+                        result += multiplied;
+                    }
+                    break;
+            }
+        }
+
+        instructions.forEach(instruction => {
+            handleInstructions(instruction)
         });
 
         console.log(result)
